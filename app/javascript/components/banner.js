1fadb2ba4f639c1e96506facca470e9643805be3
Tariq Ismail <tariqismail@gmail.com>
2020-11-08 00:14:43 +0400

    changed typed.js banner text to mocktails

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js'
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Let's make super tasty cocktails!"],
+    strings: ["Change your life", "Let's make super tasty mocktails!"],
     typeSpeed: 50,
     loop: true
   });
